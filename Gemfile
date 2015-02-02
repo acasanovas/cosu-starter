f6155af005de95b8a8cb99f9f80289e2d237428b
Albert Casanovas <albert@project2be.com>
2015-02-02 10:36:51 +0100

    Mixin added, all Susy defaults on _variables and minor bug fixes

--- Gemfile
+++ Gemfile
@@ -3,3 +3,4 @@ source "https://rubygems.org"
 gem 'sass-rails'
 gem 'compass'
 gem 'susy'
+gem 'breakpoint'
--- config.rb
+++ config.rb
@@ -5,8 +5,8 @@ css_dir = '/css'
 sass_dir = '/components/sass'
 javascripts_dir = '/js'
 images_dir = "img"
-output_style = :compressed
-#output_style = :nested
+output_style = :nested
+# output_style = :compressed
 
 relative_assets = true
 line_comments = false
